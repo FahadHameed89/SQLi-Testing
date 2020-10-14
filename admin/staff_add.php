809fd9f862ffd476b7189a81bdb8c0d11b2710a1
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-14 14:09:34 -0600

    Added staff_add.php to admin folder

---
+++ admin/staff_add.php
@@ -0,0 +1,15 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zoo - Add Staff Member</title>
+</head>
+<body>
+    <h1>Add a Staff Member!</h1>
+</body>
+</html>
