6f63172cd815847f664aa88410821a0d3413fa1d
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-14 11:30:33 -0600

    Added attempted solution

--- admin/exhibit_display.php
+++ admin/exhibit_display.php
@@ -1,7 +1,7 @@
 <?php
 require '../constants.php';
 
-$staff_members = null;
+$animal_list = null;
 
  // Create A Connection
  $connection = new mysqli(HOST, USER, PASSWORD, DATABASE);
@@ -16,24 +16,17 @@ $staff_members = null;
     }
 
     if ( 0 == $result->num_rows) {
-        $staff_members = '<tr><td colspan="4">There are no Staff members...</td></tr>';
+        $animal_list = '<tr><td colspan="4">There are no Animals...</td></tr>';
     } else {
         while( $row = $result->fetch_assoc() ) {
             // echo '<pre>';
             // print_r($row);
             // echo '</pre>';
-            $staff_members .= sprintf('
+            $animal_list .= sprintf('
             <tr>
-                <td>%s</td>
-                <td>%s</td>
-                <td>%s</td>
-                <td><a href="staff_edit.php?staff_id=%d">Edit</a></td></tr>
+                <td><a href="exhibit_display.php?exhibit_id=%d">View Animals</a></td></tr>
             ', 
-            $row['StaffID'],
-            $row['FirstName'],
-            $row['LastName'],
             $row['StaffID']
-
         );
         }
     }
@@ -49,18 +42,19 @@ $staff_members = null;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zoo Staff List</title>
+    <title>Zoo Exhibit List</title>
 </head>
 <body>
-    <h1>List of Staff Members</h1>
+    <h1>Welcome to the TechCareers Zoo!</h1>
+    <h1>Exhibits</h1>
+    <p>We currently have 1 exhibit(s) for you to visit!</p>
+    <h2>North American Animals</h2>
+    <p>A collection of animals native to North America</p>
         <table>
         <tr>
-            <th>Staff ID</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Actions</th>
+
         </tr>
-            <?php echo $staff_members ?>
+            <?php echo $animal_list ?>
         </table>
 </body>
 </html>
