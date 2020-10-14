b33ceb98d3d085731a9a90b257c9871677393c78
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-14 10:20:01 -0600

    Added row fetching association

--- index.php
+++ index.php
@@ -9,7 +9,21 @@ if ($connection->connect_error) {
     die('Connection Failed: ' . $connection->connect_error);
 }
 
-echo 'Connected Successfully, you can now perform queries';
+$sql = "SELECT * FROM Exhibit";
+
+$result = $connection->query($sql);
+
+if ($result->num_rows > 0) {
+
+    while($row = $result->fetch_assoc()) {
+        echo '<pre>';
+        print_r($row);
+        echo '</pre>'; 
+    }
+
+} else {
+        echo "there are no exhibits...";
+    }
 
 $connection->close();
 
