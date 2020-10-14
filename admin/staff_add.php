f37a828894dd288de2da92ec1a6c29065cab3ae0
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-14 14:38:33 -0600

    Solving previous problem without a PREPARED STATEMENET

--- admin/staff_add.php
+++ admin/staff_add.php
@@ -9,6 +9,27 @@
         if ( $connection->connect_errno ) {
             die('Connection Failed!: ' . $connection->connect_error);
         }
+        // PREPARED STATEMENT EXAMPLE
+        // $statement = $connection->prepare("INSERT INTO Staff (FirstName, LastName) VALUES(?,?)");
+        // $statement->bind_param("ss", $_POST['first_name'], $_POST['last_name'] );
+        // if ( $statement->execute() ) {
+        //     echo "Yay! New Staff member added!";
+        // }  else {
+        //     echo "There was an issue adding a new staff member...";
+        // }
+        // $statement->close();
+
+        // $connection->close();
+            $first_name = $connection->real_escape_string($_POST['first_name']);
+            $last_name = $connection->real_escape_string($_POST['last_name']);
+            $sql = "INSERT INTO Staff (FirstName, LastName) VALUES($first_name, $last_name)";
+            if (!$result = $connection->query($sql) ) {
+                die("Could not add a staff member to the database");
+            }
+            echo '<pre>';
+            print_r($result);
+            echo '</pre>';
+
     }
 
 ?>
