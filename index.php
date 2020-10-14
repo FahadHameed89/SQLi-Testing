e5634acbd78dae68955147d3d6e489d01c3062f6
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-14 11:40:11 -0600

    Added more progress on in-class solution

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
  // Create A Connection
  $connection = new mysqli(HOST, USER, PASSWORD, DATABASE);
  $number_of_exhibits = 0;
+ $exhibit_message = "";
+
+
 if ($connection->connect_error) {
     die('Connection Failed: ' . $connection->connect_error);
 }
@@ -16,9 +19,16 @@ $number_of_exhibits = $result->num_rows;
 if ($result->num_rows > 0) {
 
     while($row = $result->fetch_assoc()) {
-        echo '<pre>';
-        print_r($row);
-        echo '</pre>'; 
+        // echo '<pre>';
+        // print_r($row);
+        // echo '</pre>'; 
+        $exhibit_message .= sprintf('
+            <h3>%s</h3>
+            <p>%s</p>
+        ',
+        $row['ExhibitName'],
+        $row['ExhibitDescription']
+    );
     }
 
 } else {
@@ -41,7 +51,7 @@ $connection->close();
     <h1>Exhibits</h1>
     <p>We currently have <?php echo $number_of_exhibits; ?> exhibit(s) for you to visit!</p>
     <h2>North American Animals</h2>
-    <p>A collection of animals native to North America</p>
+    <?php echo $exhibit_message; ?>
      
  </body>
  </html>
