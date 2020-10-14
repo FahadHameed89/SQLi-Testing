6cbdf883b3c9fb3db639107636dd8f09c9b04e1f
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-14 11:36:32 -0600

    Begin start of in-class solution

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
 
  // Create A Connection
  $connection = new mysqli(HOST, USER, PASSWORD, DATABASE);
- 
+ $number_of_exhibits = 0;
 if ($connection->connect_error) {
     die('Connection Failed: ' . $connection->connect_error);
 }
 
-$sql = "SELECT * FROM Exhibit";
+$sql = "SELECT * FROM Exhibit WHERE NOW() BETWEEN `StartDate` AND `EndDate`";
 
 $result = $connection->query($sql);
-
+$number_of_exhibits = $result->num_rows;
 if ($result->num_rows > 0) {
 
     while($row = $result->fetch_assoc()) {
@@ -28,3 +28,20 @@ if ($result->num_rows > 0) {
 $connection->close();
 
  ?> 
+
+ <!DOCTYPE html>
+ <html lang="en">
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>TechCareers Zoo</title>
+ </head>
+ <body>
+    <h1>Welcome to the TechCareers Zoo!</h1>
+    <h1>Exhibits</h1>
+    <p>We currently have <?php echo $number_of_exhibits; ?> exhibit(s) for you to visit!</p>
+    <h2>North American Animals</h2>
+    <p>A collection of animals native to North America</p>
+     
+ </body>
+ </html>
