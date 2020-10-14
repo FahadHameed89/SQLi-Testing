046352d6c9aed8834e042610c39177818a086c91
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-10-14 15:06:49 -0600

    Added admin/index.php and admin/animal_add.php

---
+++ admin/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Admin Menu</h1>
+        <ul>
+            <li> <a href="animal_add.php">Add an Animal</a> </li>
+            <li> <a href="staff_list.php">Staff List</a> </li>
+            <li> <a href="staff_add.php">Add a Staff Member</a> </li>
+
+        </ul>
+    
+</body>
+</html>
--- admin/staff_add.php
+++ admin/staff_add.php
@@ -1,49 +1,45 @@
 <?php
     require '../constants.php';
-    if( $_POST) {
+    if( $_POST ) {
         echo '<pre>';
         print_r($_POST);
-        echo "The form was successfully submitted...! Thanks for the data sucker!";
         echo '</pre>';
+
         $connection = new MySQLi(HOST, USER, PASSWORD, DATABASE);
-        if ( $connection->connect_errno ) {
-            die('Connection Failed!: ' . $connection->connect_error);
+        if( $connection->connect_errno ) {
+            die('Connection failed: ' . $connection->connect_error);
         }
-        // PREPARED STATEMENT EXAMPLE
+        // PREPARED STATEMENT
         // $statement = $connection->prepare("INSERT INTO Staff (FirstName, LastName) VALUES(?,?)");
-        // $statement->bind_param("ss", $_POST['first_name'], $_POST['last_name'] );
-        // if ( $statement->execute() ) {
-        //     echo "Yay! New Staff member added!";
-        // }  else {
-        //     echo "There was an issue adding a new staff member...";
+        // $statement->bind_param("ss", $_POST['first_name'], $_POST['last_name']);
+        // if( $statement->execute() ) {
+        //     echo "Yay! We've added a staff member to the database";
+        // } else {
+        //     echo "There was a problem adding a staff member to the database";
         // }
         // $statement->close();
 
-        // $connection->close();
-            $first_name = $connection->real_escape_string($_POST['first_name']);
-            $last_name = $connection->real_escape_string($_POST['last_name']);
-            $sql = "INSERT INTO Staff (FirstName, LastName) VALUES($first_name, $last_name)";
-            if (!$result = $connection->query($sql) ) {
-                die("Could not add a staff member to the database");
-            }
-            echo '<pre>';
-            print_r($result);
-            echo '</pre>';
+        // WITHOUT A PREPARED STATEMENT
+        $first_name = $connection->real_escape_string($_POST['first_name']);
+        $last_name = $connection->real_escape_string($_POST['last_name']);
+        $sql = "INSERT INTO Staff (FirstName, LastName) VALUES('$first_name', '$last_name')";
+        if( !$result = $connection->query($sql) ) {
+            die("Could not add staff member to the database");
+        }
 
+        $connection->close();
     }
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zoo - Add Staff Member</title>
+    <title>Zoo - Add Staff</title>
 </head>
 <body>
-    <h1>Add a Staff Member!</h1>
-    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-sata">
+    <h1>Add a staff member</h1>
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" enctype="multipart/form-data">
     <p>
         <label for="first_name">First Name</label>
         <input type="text" name="first_name" id="first_name">
@@ -53,9 +49,8 @@
         <input type="text" name="last_name" id="last_name">
     </p>
     <p>
-        <input type="submit" value="Add new staff member!">
+        <input type="submit" value="Add new staff member">
     </p>
-
-    </form> 
+    </form>
 </body>
 </html>
